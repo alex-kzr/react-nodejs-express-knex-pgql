0f74c44a944ab9c977cdc083a71e5350d032046f
Alex-SK <aleksey.kozyura@gmail.com>
2020-05-24 17:47:37 +1000

    Add NavBar component

---
+++ client/src/component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import TopNav from './TopNav';
+
+function App() {
+  return (
+    <div>
+      <TopNav />
+      <div className="container">
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/component/TopNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TopNav = () => {
+    return(
+        <nav className="navbar navbar-dark bg-dark">
+            <span className="navbar-brand">ToDo</span>
+        </nav>
+    );
+}
+
+export default TopNav;
